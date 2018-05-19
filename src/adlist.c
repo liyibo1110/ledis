1ef9f79e516eb191f28872744850bb2fcbe9d2d2
liyibo1110 <liyibo1110@sina.com>
2018-05-19 23:49:49 +0800

    增加接口函数

--- src/adlist.c
+++ src/adlist.c
@@ -80,3 +80,97 @@ void listDelNode(list *list, listNode *node){
     free(node);
     list->len--;
 }
+
+listIter *listGetIterator(list *list, int direction){
+    listIter *iter = malloc(sizeof(*iter)); 
+    if(iter == NULL) return NULL;
+    //指定第一个元素
+    if(direction == AL_START_HEAD){
+        iter->next = list->head;
+    }else{
+        iter->next = list->tail;
+    }
+    iter->direction = direction;
+    return iter;    //返回的iter是需要自己在外面释放的
+}
+
+listNode *listNextElement(listIter *iter){
+    listNode *current = iter->next;
+    if(current != NULL){    //有元素则需要移动迭代器，指向下一个元素
+        if(iter->direction == AL_START_HEAD){
+            iter->next = current->next;
+        }else{
+            iter->next = current->prev;
+        }
+    }
+    return current;
+}
+
+void listReleaseIterator(listIter *iter){
+    free(iter); //释放自己就够了，不需要释放node
+}
+
+list *listDup(list *orig){
+    list *copy = listCreate();
+    if(copy == NULL) return NULL;
+    //先复制函数
+    copy->dup = orig->dup;
+    copy->free = orig->free;
+    copy->match = orig->match;
+    listIter *iter = listGetIterator(orig, AL_START_HEAD);
+
+    listNode *node;
+    while((node = listNextElement(iter)) != NULL){
+        void *value;
+        if(copy->dup){  //如果有自定义的dup函数
+            value = copy->dup(node->value);
+            if(value == NULL){  //只是进行了很粗略的检查
+                listRelease(copy);
+                listReleaseIterator(iter);
+                return NULL;
+            }
+        }else{
+            value = node->value;
+        }
+        //开始添加节点
+        if(listAddNodeTail(copy, value) == NULL){
+            listRelease(copy);
+            listReleaseIterator(iter);
+            return NULL;
+        }
+    }
+    listReleaseIterator(iter);
+    return copy;
+}
+
+listNode *listSearchKey(list *list, void *key){
+    listIter *iter = listGetIterator(list, AL_START_HEAD);
+    listNode *node;
+    while((node = listNextElement(iter)) != NULL){
+        if(list->match){    //如果有自定义的match函数
+            if(list->match(node->value, key)){
+                listReleaseIterator(iter);
+                return node;
+            }
+        }else{
+            if(key == node->value){
+                listReleaseIterator(iter);
+                return node;
+            }
+        }
+    }
+    listReleaseIterator(iter);
+    return NULL;
+}
+
+listNode *listIndex(list *list, int index){
+    listNode *node;
+    if(index < 0){  //可以支持为负的下标
+        index = (-index)-1;
+        node = list->tail;
+        while(index-- && node) node = node->prev;
+    }else{
+        node = list->head;
+        while(index-- && node) node = node->next;
+    }
+}
