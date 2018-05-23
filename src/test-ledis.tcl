9d7ed1d882937705b0d8808265a52f0ba2ba89c9
liyibo1110 <liyibo1110@sina.com>
2018-05-23 23:56:19 +0800

    增加辅助函数

--- src/test-ledis.tcl
+++ src/test-ledis.tcl
@@ -2,8 +2,21 @@
 set ::passed 0  ;#全局统计数
 set ::failed 0  ;#全局统计数
 
+proc test {name code okpattern} {
+    puts -nonewline [format "%-70s " name]
+    flush stdout
+    set retval [uplevel 1 $code]    ;#在全局环境执行code
+    if {$okpattern eq $retval || [string match $okpattern $retval]} {
+        puts "PASSED"
+        incr ::passed
+    } else {
+        puts "!! ERROR expected '$okpattern' but got '$retval'"
+        incr ::failed
+    }
+}
+
 proc main {server port} {
-    #set fd [ledis_connect $server $port]
+    set fd [ledis_connect $server $port]
     puts "\n[expr $::passed+$::failed] tests, $::passed passed, $::failed failed"
     if{$::failed > 0} {
         puts "\n*** WARNING!!! $::failed FAILED TESTS ***\n"
@@ -15,6 +28,18 @@ proc ledis_connect {server port} {
     set fd [socket $server $port]
     fconfigure $fd -translation binary
     return $fd
+}
+
+#在fd中输出buf不换行
+proc ledis_write {fd buf} {
+    puts -nonewline $fd $buf
+}
+
+#在fd中输出buf并换行
+proc ledis_writenl {fd buf} {
+    ledis_write $fd $buf
+    ledis_write $fd "\r\n"
+    flush $fd
 }
 
 #开始执行
